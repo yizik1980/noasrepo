d7363552133fc97b00678966f4f537828526dabb
yizik1980 <syizikm@gmail.com>
2021-04-22 17:59:50 +0300

    http to https more secure calls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  autocomplet:'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
-  fiveDays:'http://dataservice.accuweather.com/forecasts/v1/daily/5day/',
+  autocomplet:'https://dataservice.accuweather.com/locations/v1/cities/autocomplete',
+  fiveDays:'https://dataservice.accuweather.com/forecasts/v1/daily/5day/',
   apiCode:'apikey=eQ54Bwy7RIuZeAn4J7LQkpAzh1AKnLdN&metric=true',
   hebLang:'&language=he-il',
   iconsUrl: 'https://apidev.accuweather.com/developers/Media/Default/WeatherIcons/',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  autocomplet:'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
-  fiveDays:'http://dataservice.accuweather.com/forecasts/v1/daily/5day/',
+  autocomplet:'https://dataservice.accuweather.com/locations/v1/cities/autocomplete',
+  fiveDays:'https://dataservice.accuweather.com/forecasts/v1/daily/5day/',
   apiCode:'apikey=eQ54Bwy7RIuZeAn4J7LQkpAzh1AKnLdN&metric=true',
   hebLang:'&language=he-il',
   iconsUrl: 'https://apidev.accuweather.com/developers/Media/Default/WeatherIcons/',
